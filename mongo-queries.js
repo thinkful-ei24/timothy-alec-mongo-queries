8426c927c2b6ac9180c564c56814eb77752cf687
Timothy Chang <superfluous11@gmail.com>
2018-09-17 11:59:39 -0700

    completed first 10 exercises

---
+++ mongo-queries.js
@@ -0,0 +1,49 @@
+
+// // 1. displays all documents
+//  db.notes.find();
+
+// // 2. display all documents and formats with pretty()
+// db.notes.find().pretty();
+
+// 3.
+// db.notes.find(
+//     {},
+//     { title: 1, content: 1}
+// ).pretty();
+
+//4. exclude _id field
+// db.notes.find(
+//     {},
+//     { title: 1, content: 1, _id: 0}
+// ).pretty();
+
+// 5. display only title field and sort results by _id in descending order
+// db.notes.find(
+//     {},
+//     {title: 1, _id: 0}
+// ).sort({_id: -1});
+
+// 6. 
+// db.notes.find({title: '5 life lessons learned from cats'}).pretty();
+
+// 7. display first five documents
+// db.notes.find().limit(5).pretty();
+
+// 8. display next five after skipping first five
+// db.notes.find().skip(5).limit(5).pretty();
+
+// 9. display total number documents in collection notes
+// db.notes.find().count();
+
+// 10. createdAt date greater than Jan 01 2018
+// db.notes.find(
+//     {
+//         createdAt: {
+//             $gt: new Date('Jan 01 2018')
+//         }
+//     },
+//     {
+//         title: 1,
+//         createdAt: 1
+//     }
+// ).pretty();
